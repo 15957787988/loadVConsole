87d9639ee2b08385ed78f3eb640160addcae9e6d
zhangzeng <zhangzeng@zhangzengdeMacBook-Pro.local>
2020-06-08 20:24:37 +0800

    t

---
+++ debug的副本.js
@@ -0,0 +1,46 @@
+var lastTime = 0
+var count = 0
+function debug () {
+  document.body.addEventListener('touchstart', (e) => {
+    var touches = e.touches
+    if (touches.length === 2) {
+      const currentTime = new Date().getTime()
+      if ((currentTime - lastTime) < 500) {
+        count += 1
+      }
+      lastTime = new Date().getTime()
+      if (count > 3 && window.vConsole) {
+        removeScript()
+      }
+      if (count > 10) {
+        count = 0
+        loadVConsole()
+      }
+    }
+  })
+}
+function loadVConsole () {
+  var head = document.getElementsByTagName('head')[0]
+  var script = document.createElement('script')
+  script.src = 'https://d.2dfire.com/file/js/vconsole.3.3.0.js'
+  script.addEventListener('load', function () {
+    var script2 = document.createElement('script')
+    script2.innerHTML = 'window.vConsole = new VConsole()'
+    head.appendChild(script2)
+  })
+  head.appendChild(script)
+}
+
+function removeScript () {
+  var scripts = document.getElementsByTagName('script')
+  var __vconsole = document.getElementById('__vconsole')
+  for (var i = 0; i < scripts.length; i++) {
+    if (scripts[i] && scripts[i].getAttribute('src') != null && scripts[i].getAttribute('src').indexOf('vconsole.3.3.0.js') !== -1) {
+      scripts[i].parentNode.removeChild(scripts[i])
+      __vconsole.remove()
+      delete window.vConsole
+    }
+  }
+}
+
+debug()
